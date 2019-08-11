39b16d83252da0990e5a3d9b37b97ea0cf8ad342
Guilherme Siani <guilherme@siani.com.br>
2019-08-10 21:25:11 -0300

    Add LinearSearch ref to test

--- TestKnowledgeTree/testSampleClass.cpp
+++ TestKnowledgeTree/testSampleClass.cpp
@@ -12,24 +12,10 @@
 #include <array>
 
 TEST_CASE("Testing Sample Class") {
+    LinearSearch linearSearch;
   
-  SECTION("setting the str") {
-      LinearSearch linearSearch;
-      std::array<int, 5> args = {3,5,6,1,7};
-      int element = 3;
-      CHECK(linearSearch.inArray(element, args));
-  }
-  
-  SECTION("test bigStr") {
-//    sc.setStr("TestStr");
-//    REQUIRE(sc.bigStr() == "TestStr : 8");
-      CHECK_FALSE(false);
-  }
-  
-  SECTION("Test doubles") {
-//    sc.setD(1);
-//    CHECK(sc.getD() == 1);
-//    sc.setD(1.0/3.0);
-//    CHECK(sc.getD() == Approx(0.33333)); // Nice
-  }
+    SECTION("setting the str") {
+        std::array<int, 5> args = {3,5,6,1,7};
+        CHECK(linearSearch.inArray(3, args));
+    }
 }
