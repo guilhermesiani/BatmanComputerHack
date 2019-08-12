1653ce81495f601a8abe0077e0f0650c57a53a7a
Guilherme Siani <guilherme@siani.com.br>
2019-08-11 21:43:57 -0300

    Add failing tests to binary search

--- TestKnowledgeTree/Algorithms/testBinarySearch.cpp
+++ TestKnowledgeTree/Algorithms/testBinarySearch.cpp
@@ -18,8 +18,18 @@ TEST_CASE("Testing Binary Search") {
         CHECK(binarySearch.iteractive(vector, 8, 7) == 5);
     }
     
+    SECTION("iteractive search fail") {
+        int vector[7] = {3,4,5,6,7,8,9};
+        CHECK(binarySearch.iteractive(vector, 2, 7) == -1);
+    }
+    
     SECTION("recursive search") {
         int vector[7] = {3,4,5,6,7,8,9};
         CHECK(binarySearch.recursive(8, vector, 0, 6) == 5);
+    }
+    
+    SECTION("recursive search fail") {
+        int vector[7] = {3,4,5,6,7,8,9};
+        CHECK(binarySearch.recursive(10, vector, 0, 6) == -1);
     }
 }
