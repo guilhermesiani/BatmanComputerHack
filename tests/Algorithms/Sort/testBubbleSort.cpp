83cc28699b6b96f3016eba81b70b0fe70c944004
Guilherme Siani <guilherme@siani.com.br>
2019-08-17 20:28:08 -0300

    Fix segmentation fault on test

--- tests/Algorithms/Sort/testBubbleSort.cpp
+++ tests/Algorithms/Sort/testBubbleSort.cpp
@@ -8,13 +8,7 @@ TEST_CASE("Testing Bubble Sort") {
     
     SECTION("Should sort array") {
         int size = 5;
-        int *numbers;
-        numbers = (int*)malloc(size);
-        numbers[0] = 3;
-        numbers[1] = 5;
-        numbers[2] = 1;
-        numbers[3] = 2;
-        numbers[4] = 4;
+        int numbers[5] = {3,5,1,2,4};
         bubbleSort.sort(numbers, size - 1);
         for (int i = 0; i < size - 1; i++) {
             CHECK(numbers[i] == (i + 1));
