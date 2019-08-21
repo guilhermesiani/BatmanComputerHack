465f71f2fb1651a2542e25b3c310294916cde5f0
Guilherme Siani <guilherme@siani.com.br>
2019-08-20 22:09:25 -0300

    Creating Character builder

---
+++ tests/DesignPatterns/Builder/RPGCharacter/testCharacterBuilder.cpp
@@ -0,0 +1,11 @@
+#include "../../../catch.hpp"
+
+#include "../../../../src/DesignPatterns/Builder/RPGCharacter/CharacterBuilder.cpp"
+
+TEST_CASE("Testing RPG Character Builder") {
+    CharacterBuilder builder;
+
+    SECTION("Testing build a complete Palading character") {
+        // builder
+    }
+}
